47c6c572bc912da6c103ca9773b1eda8f5c620a8
harshjiop <hk8051871496@gmail.com>
2023-08-25 13:01:11 +0530

    tailwind

---
+++ Client/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import CustomRouts from './Componants/CustomRouts'
+
+function App() {
+
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline bg-blue-900">Hello world!XApp</h1>
+  <CustomRouts/>
+    </>
+  )
+}
+
+export default App
---
+++ Client/src/Componants/CustomRouts.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function CustomRouts() {
+  return (
+    <h1 className="text-3xl font-bold underline bg-blue-900">Hello world!X</h1>
+  );
+}
+
+export default CustomRouts;
